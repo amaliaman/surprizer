d95725e24e1f5e48b0000be123f4d4103019ce2c
Amalia Man <amaliagilad@gmail.com>
2018-10-28 10:00:18 +0200

    Fix

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use('/greetings', greetingsApi);
 app.use('/chat', chatApi);
 
 // Production app
-if (app.get('env') === 'production') {
+if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'build')));
 	app.get('/*', function (req, res) {
 		res.sendFile(path.join(__dirname, 'build', 'index.html'));
