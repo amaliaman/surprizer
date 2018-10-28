b537a20052c1d133052ffe6cbfb3c4c4101ae543
Amalia Man <amaliagilad@gmail.com>
2018-10-28 10:33:43 +0200

    Remove buiod from gitignore

---
+++ build/precache-manifest.2671f16f083f404ee6b4bf8e4b7a9fd8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5331cb6372b6c0d8baf2dd7e200498c",
+    "url": "/static/media/OpenSans-Bold.f5331cb6.ttf"
+  },
+  {
+    "revision": "fe21a9fc1c59526aee3b",
+    "url": "/static/css/main.f8270b16.chunk.css"
+  },
+  {
+    "revision": "c31c6e18c05f23169024d27fb279de5f",
+    "url": "/static/media/party-bg.c31c6e18.jpg"
+  },
+  {
+    "revision": "37ad0419ac09204bb1a4",
+    "url": "/static/js/1.37ad0419.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/static/media/OpenSans-Regular.d7d5d458.ttf"
+  },
+  {
+    "revision": "fe21a9fc1c59526aee3b",
+    "url": "/static/js/main.fe21a9fc.chunk.js"
+  },
+  {
+    "revision": "8826636b335d407991f5f2006e891cbe",
+    "url": "/static/media/events-bg.8826636b.jpg"
+  },
+  {
+    "revision": "c37757eb613b1bd626ff771c6bf93632",
+    "url": "/static/media/about-bg.c37757eb.jpg"
+  },
+  {
+    "revision": "874a1043daa2162240faec5370f15b45",
+    "url": "/static/media/main_bg-min.874a1043.jpg"
+  },
+  {
+    "revision": "37ad0419ac09204bb1a4",
+    "url": "/static/css/1.dc2ad469.chunk.css"
+  },
+  {
+    "revision": "aa4ced69878c5071ecb0068cb19c0715",
+    "url": "/index.html"
+  }
+];
