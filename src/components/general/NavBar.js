635600945fcddbcc1d699e1e6857d529e9dbd382
Amalia Man <amaliagilad@gmail.com>
2018-10-31 21:57:34 +0200

    Fix burger

--- src/components/general/NavBar.js
+++ src/components/general/NavBar.js
@@ -1,40 +1,44 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import LoginControl from '../login/LoginControl';
+import { observable, action } from 'mobx';
+import { observer } from 'mobx-react';
 
+@observer
 class NavBar extends Component {
+    @observable isChecked = false;
+
     constructor() {
         super();
         this.links = [
-            // { title: '🎉', to: '/' },
-            // { title: 'Surprizer', to: '/' },
             { title: 'My Events', to: '/events' },
             { title: 'About', to: '/about' }
         ];
     }
+
+    @action handleCheck = e => {
+        this[e.target.name] = e.target.checked;
+    };
+
+    @action handleClick = e => {
+        this.isChecked = false;
+    };
 
     render() {
         return (
             <header className="header">
                 <div className='outer-wrapper'>
                     <div className='container'>
-                        <NavLink activeClassName='active-link'  to="/" className="logo" exact><span role="img" aria-label="Surprizer title">🎉</span> Surprizer</NavLink>
-                        <input className="menu-btn" type="checkbox" id="menu-btn" />
+                        <NavLink onClick={this.handleClick} activeClassName='active-link' to="/" className="logo" exact><span role="img" aria-label="Surprizer title">🎉</span> Surprizer</NavLink>
+                        <input name='isChecked' checked={this.isChecked} onChange={this.handleCheck} className="menu-btn" type="checkbox" id="menu-btn" />
                         <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
                         <ul className="menu">
-                            {this.links.map((l, i) => <li key={i}><NavLink to={l.to} activeClassName='active-link' exact>{l.title}</NavLink></li>)}
-                            <li><LoginControl /></li>
+                            {this.links.map((l, i) => <li onClick={this.handleClick} key={i}><NavLink to={l.to} activeClassName='active-link' exact>{l.title}</NavLink></li>)}
+                            <li onClick={this.handleClick}><LoginControl /></li>
                         </ul>
                     </div>
                 </div>
             </header>
-
-            // <div className='outer-wrapper nav-wrapper'>
-            //     <div className='container nav-container'>
-            //         {this.links.map((l, i) => <NavLink key={i} to={l.to} activeClassName='active-link' exact>{l.title}</NavLink>)}
-            //         <LoginControl />
-            //     </div>
-            // </div>
         );
     }
 }
